fb5e509f0a6bd18499e15d41d64d35693cd1a101
Spoiledpay <55815349+Spoiledpay@users.noreply.github.com>
2020-07-10 22:25:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,21 @@ function createWindow () {
   let win = new BrowserWindow({
     width: 800,
     height: 600,
+    show:false,
     webPreferences: {
       nodeIntegration: true
+      
     }
   })
 
+  
   // e carregar o index.html do aplicativo.
   win.loadFile('index.html')
+  
+  win.once("ready-to-show", ()=>{
+    win.show();
+  })
+
 }
 
 app.whenReady().then(createWindow)
